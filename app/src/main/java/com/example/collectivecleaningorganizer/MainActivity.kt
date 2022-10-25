ec782d2faad728c85dba2b4cbb76b99e104bb95b
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-10-26 00:25:22 +0200

    Wrote some code to start the addTaskActivity for testing purposes

--- app/src/main/java/com/example/collectivecleaningorganizer/MainActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.collectivecleaningorganizer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        /*
+        val intentAddTask: Intent = Intent(this,AddTaskActivity::class.java)
+        startActivity(intentAddTask)
+
+         */
+
     }
 }
