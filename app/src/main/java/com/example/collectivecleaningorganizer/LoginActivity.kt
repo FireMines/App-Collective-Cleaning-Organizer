eb508ade90145d9feac64d84a258180e2084fcb1
Christopher Kindlien <toffie2511@outlook.com>
2022-10-30 23:08:48 +0100

    Created intent to move from LoginActivity.kt to ForgotPasswordActivity.

--- app/src/main/java/com/example/collectivecleaningorganizer/LoginActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.collectivecleaningorganizer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Starts ForgotPasswordActivity when clicking on forgotPassword
+        forgotPassword.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, ForgotPasswordActivity::class.java))
+        }
+
     }
 }
+
