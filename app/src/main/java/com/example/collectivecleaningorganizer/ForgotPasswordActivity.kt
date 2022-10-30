297f53cc6293efbe9e38c8fd7f42f758377fe374
Christopher Kindlien <toffie2511@outlook.com>
2022-10-30 22:16:37 +0100

    Changed name from ForgotPassword to ForgotPasswordActivity

--- app/src/main/java/com/example/collectivecleaningorganizer/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/ForgotPasswordActivity.kt
@@ -3,7 +3,7 @@ package com.example.collectivecleaningorganizer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ForgotPassword : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
