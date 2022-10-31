b5b1317498fadd29c9dff90741eb432fdc73970f
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-10-31 17:49:22 +0100

    Added temporarily testing code done to see if things are added to the firestore database

--- app/src/main/java/com/example/collectivecleaningorganizer/MainActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.collectivecleaningorganizer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +22,21 @@ class MainActivity : AppCompatActivity() {
         startActivity(newIntent)
 
          */
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                println("added $documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                println("failed ")
+            }
     }
 }
