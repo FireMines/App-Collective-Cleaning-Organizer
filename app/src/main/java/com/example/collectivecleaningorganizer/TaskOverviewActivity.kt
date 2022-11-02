993734304283e40cbffaf71d013ef84b64641176
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-11-02 13:10:57 +0100

    [#15] Edited how the tasks looked in overview and made add button enter add page

--- app/src/main/java/com/example/collectivecleaningorganizer/TaskOverviewActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/TaskOverviewActivity.kt
@@ -1,11 +1,19 @@
 package com.example.collectivecleaningorganizer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_task_overview.*
 
 class TaskOverviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_overview)
+
+        // Starts ForgotPasswordActivity when clicking on forgotPassword
+        add_btn.setOnClickListener {
+            startActivity(Intent(this, AddTaskActivity::class.java))
+        }
     }
 }
