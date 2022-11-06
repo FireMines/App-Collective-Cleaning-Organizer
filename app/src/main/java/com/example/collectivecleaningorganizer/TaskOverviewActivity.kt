3fbc5647e3c61bed5e2f129b66dde7434ecf538c
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-07 00:30:55 +0100

    Made it so that the intent contains the user id when starting the AddTaskActivity

--- app/src/main/java/com/example/collectivecleaningorganizer/TaskOverviewActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/TaskOverviewActivity.kt
@@ -1,8 +1,13 @@
 package com.example.collectivecleaningorganizer
 
+import android.content.ContentValues.TAG
 import android.content.Intent
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_task_overview.*
 
@@ -10,10 +15,14 @@ class TaskOverviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_overview)
+        val userID = intent.getStringExtra("uid")
+
+        val intentAddTaskPage: Intent = Intent(this,AddTaskActivity::class.java)
+        intentAddTaskPage.putExtra("uid",userID)
 
         // Starts ForgotPasswordActivity when clicking on forgotPassword
         add_btn.setOnClickListener {
-            startActivity(Intent(this, AddTaskActivity::class.java))
+            startActivity(intentAddTaskPage)
         }
     }
 }
