179365b510e21750d206a648e0f427ee23235f43
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-11-07 23:34:38 +0100

    Added taskmodel for when you need to list up all tasks

---
+++ app/src/main/java/com/example/collectivecleaningorganizer/TaskModel.kt
@@ -0,0 +1,43 @@
+package com.example.collectivecleaningorganizer
+
+import android.os.Parcel
+import android.os.Parcelable
+
+
+data class TaskModel(
+    var name: String,
+    var dueDate: String,
+    var description: String
+
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(dueDate)
+        parcel.writeString(description)
+        //parcel.writeInt(picture)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<TaskModel> {
+        override fun createFromParcel(parcel: Parcel): TaskModel {
+            return TaskModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<TaskModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
+
+
+
