126d0d2e47546b4a6ff03779048824d143f391a6
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-07 04:26:09 +0100

    Created a new kt file for EditTask

---
+++ app/src/main/java/com/example/collectivecleaningorganizer/EditTaskActivity.kt
@@ -0,0 +1,18 @@
+package com.example.collectivecleaningorganizer
+
+import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_task.*
+
+class EditTaskActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+        taskPageTitle.text = "Edit Task Page"
+        saveButton.text = "Save"
+
+
+    }
+}
