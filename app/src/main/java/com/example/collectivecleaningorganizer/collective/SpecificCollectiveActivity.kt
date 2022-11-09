0d3901d40162f0721cb3eecbaab74fa8b48b7ef7
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-10 00:16:32 +0100

    Created an activity used to show detailed information about the collective the user is in

---
+++ app/src/main/java/com/example/collectivecleaningorganizer/collective/SpecificCollectiveActivity.kt
@@ -0,0 +1,12 @@
+package com.example.collectivecleaningorganizer.collective
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.collectivecleaningorganizer.R
+
+class SpecificCollectiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_specific_collective)
+    }
+}
