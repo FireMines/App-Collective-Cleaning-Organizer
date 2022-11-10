09938e406003718cdc13be2bb8415ca8d969d562
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-10 05:09:59 +0100

    Created a function that retrieves user data from DB and stores it to a mutable map

--- app/src/main/java/com/example/collectivecleaningorganizer/LoginActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/LoginActivity.kt
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.example.collectivecleaningorganizer.collective.CollectiveActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     // declares instance of firebaseAuth
     private lateinit var auth: FirebaseAuth
-
+    private val db = Firebase.firestore
+    var userData = mutableMapOf<String,DocumentSnapshot>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -43,13 +48,30 @@ class LoginActivity : AppCompatActivity() {
             if (task.isSuccessful) {
                 Log.d("log in", "login:success")
                 Toast.makeText(this, "Successful login", Toast.LENGTH_SHORT).show()   // displays message to user
-            /**
-                startActivity(Intent(this, ForgotPasswordActivity::class.java))
-                Needs different activity here. Used ForgotPasswordActivity as test
-            */
-            val intentTaskPage: Intent = Intent(this,TaskOverviewActivity::class.java)
-                intentTaskPage.putExtra("uid",task.result.user?.uid)
-                startActivity(intentTaskPage)
+
+                //DB request to retrieve amy user data
+                db.collection("users").document(task.result.user?.uid.toString()).get().addOnSuccessListener { e ->
+                    //Adding the userData to a mutable map
+                    userData[e.id] = e
+
+                    val intent: Intent
+                    //Checking if the user is apart of a collective or not
+                    if (e.data?.get("collectiveID") == null) {
+                        //Start the CollectiveActivity
+                        intent = Intent(this,CollectiveActivity::class.java)
+                    }
+                    else {
+                        //Start the TaskOverview activity
+                        intent = Intent(this,TaskOverviewActivity::class.java)
+                    }
+                    //Adding the userID to the intent
+                    intent.putExtra("uid",task.result.user?.uid)
+
+                    //Starting the activity
+                    startActivity(intent)
+                }
+
+
             } else {
                 Log.w("log in", "login:failure")
                 Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
