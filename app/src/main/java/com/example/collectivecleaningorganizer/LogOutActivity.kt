f7320900ec3b2d6df630da81fab0ab9d9f3f4be3
Christopher Kindlien <toffie2511@outlook.com>
2022-11-13 15:51:25 +0100

    Implemented logic for backbutton

--- app/src/main/java/com/example/collectivecleaningorganizer/LogOutActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/LogOutActivity.kt
@@ -1,8 +1,12 @@
 package com.example.collectivecleaningorganizer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.collectivecleaningorganizer.ui.task.TaskOverviewActivity
+import kotlinx.android.synthetic.main.activity_add_task.*
+import kotlinx.android.synthetic.main.activity_log_out.*
 
 class LogOutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +20,19 @@ class LogOutActivity : AppCompatActivity() {
             return
         }
 
+        // takes user back to task overview
+        backToTaskOverviewButton.setOnClickListener {
+            val intentTaskPage: Intent = Intent(this, TaskOverviewActivity::class.java)
+            intentTaskPage.putExtra("uid",userId)
+            startActivity(intentTaskPage)
+        }
+
+
+        //back_btn.setOnClickListener{
+        //    val intentTaskPage: Intent = Intent(this, TaskOverviewActivity::class.java)
+        //    intentTaskPage.putExtra("uid",userID)
+        //    startActivity(intentTaskPage)
+        //}
+
     }
-
-
 }
