1e3b3386b934776ee2ff131eb73112142d001e6d
Christopher Kindlien <toffie2511@outlook.com>
2022-11-13 15:41:54 +0100

    The logout activity now gets uid from task overview activety

--- app/src/main/java/com/example/collectivecleaningorganizer/LogOutActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/LogOutActivity.kt
@@ -2,10 +2,21 @@ package com.example.collectivecleaningorganizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LogOutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_out)
+
+        // gets user id passed via intent from TaskOverviewActivity.kt
+        val userId = intent.getStringExtra("uid")
+        if (userId == null) {
+            Log.d("TaskOverview: Error", "the userID is null")
+            return
+        }
+
     }
+
+
 }
