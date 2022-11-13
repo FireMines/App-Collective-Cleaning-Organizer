256aa5a4bed044af2278d6c5b762494776cf4574
Christopher Kindlien <toffie2511@outlook.com>
2022-11-13 16:13:30 +0100

    Created logout functionality

--- app/src/main/java/com/example/collectivecleaningorganizer/LogOutActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/LogOutActivity.kt
@@ -4,8 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.example.collectivecleaningorganizer.ui.login.LoginActivity
 import com.example.collectivecleaningorganizer.ui.task.TaskOverviewActivity
-import kotlinx.android.synthetic.main.activity_add_task.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_log_out.*
 
 class LogOutActivity : AppCompatActivity() {
@@ -27,12 +30,17 @@ class LogOutActivity : AppCompatActivity() {
             startActivity(intentTaskPage)
         }
 
+        // when user clicks on logout button
+        logOutButton.setOnClickListener {
+            logout()
+        }
+    }
 
-        //back_btn.setOnClickListener{
-        //    val intentTaskPage: Intent = Intent(this, TaskOverviewActivity::class.java)
-        //    intentTaskPage.putExtra("uid",userID)
-        //    startActivity(intentTaskPage)
-        //}
+    // logs out the user
+    private fun logout() {
+        Firebase.auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
 
+        Toast.makeText(this, "Logout Successful",Toast.LENGTH_LONG).show()
     }
 }
