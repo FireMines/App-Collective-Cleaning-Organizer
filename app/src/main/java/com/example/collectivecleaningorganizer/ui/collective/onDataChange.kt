bc79adf04e43c0ae3f5fd476bd3aaea93e378b6e
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-14 04:20:19 +0100

    Created a custom listener used to listen when a db task is successful

--- app/src/main/java/com/example/collectivecleaningorganizer/ui/collective/onDataChange.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/ui/collective/onDataChange.kt
@@ -4,4 +4,8 @@ import android.widget.Spinner
 
 interface onDataChange {
     fun collectiveMemberRolesChanged(updatedMembersMap: MutableMap<String, String>)
+
+}
+interface ResultListener {
+    fun onResult(isAdded: Boolean)
 }
