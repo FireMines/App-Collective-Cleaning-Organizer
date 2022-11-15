9d6b366f12d8d69fee49a38ffcc497c915d17f37
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-15 21:40:29 +0100

    Finished the createTask() function and wrote comments on all functions

--- app/src/main/java/com/example/collectivecleaningorganizer/ui/task/AddTaskActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/ui/task/AddTaskActivity.kt
@@ -12,18 +12,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
 import android.widget.*
+import androidx.core.view.size
 import com.example.collectivecleaningorganizer.Database
 import com.example.collectivecleaningorganizer.R
-import com.example.collectivecleaningorganizer.ui.collective.CollectiveActivity
-import com.example.collectivecleaningorganizer.ui.collective.ResultListener
-
 import com.example.collectivecleaningorganizer.userCollectiveData
 import com.example.collectivecleaningorganizer.userData
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_add_task.*
-import kotlinx.android.synthetic.main.popup_with_edittext.view.*
-import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -43,43 +39,28 @@ class AddTaskActivity : AppCompatActivity() {
             return
         }
 
-
         saveOrCreateButton.setOnClickListener{
             //Calling the function used to create the task by adding it to the DB
             createTask()
         }
 
-
+        //A click listener for the back button.
         back_btn.setOnClickListener{
-
+            //Finishes this (AddTaskActivity) and goes back to the TaskOverViewActivity
             this.finish()
-
-
         }
 
-        //Log.e("test", taskCategories.selectedItem.toString())
-        /*
-        taskCategories.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                Log.e("test", "${p0?.getItemAtPosition(p2)}")
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                return
-            }
-
-        }
-
-         */
-
+        //Calling the showCategoriesToChooseFrom() functon to populate the spinner and show the collective categories
         showCategoriesToChooseFrom()
+        //Calling the showMembersToAssign() to populate the listview with collective members
         showMembersToAssign()
 
     }
-    fun showCategoriesToChooseFrom() {
-        //Creating a variable for an array list
 
-
+    /**
+     * This is a function that populate the spinner with the id "taskCategories" with categories if there are some
+     */
+    private fun showCategoriesToChooseFrom() {
         //Checking if the categories list doesn't exist in the cached data
         if (categoriesArrayListFromCache != null) {
             Log.d(tag, "There are no category data in the cached data")
@@ -87,15 +68,17 @@ class AddTaskActivity : AppCompatActivity() {
             categoriesArrayList = categoriesArrayListFromCache
         }
 
-
         //Creating an ArrayAdapter with the "simple_dropdown_item_1line" layout and adding the categoriesArrayList to the adapter
         val categoriesAdapter = ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,categoriesArrayList)
 
         //Assigning the adapter to the Spinner with the id "taskCategories"
         taskCategories.adapter = categoriesAdapter
-
     }
-    fun showMembersToAssign() {
+
+    /**
+     * A function that shows all the members of the collective. It is shown in the listview with the id "assignCollectiveMembersListView"
+     */
+    private fun showMembersToAssign() {
         //Retrieving the collective members map from the stored cache data
         val collectiveMembersMap : MutableMap<String,String> = userCollectiveData[0]?.data?.get("members") as MutableMap<String, String>
         //Creating an arraylist for the keys from the collective members map
@@ -105,17 +88,28 @@ class AddTaskActivity : AppCompatActivity() {
         val membersAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,collectiveMemberArrayList)
         //Assigning the adapter to the ListView with the id "assignCollectiveMembersListView"
         assignCollectiveMembersListView.adapter = membersAdapter
+
     }
     fun createNewCategory(view: View) {
-
+        //Initializing an EditText widget
         val inputEditTextField = EditText(this)
+        //Initializing a variable to get the text value from the input Edit text field
+        val userInput = inputEditTextField.text.toString()
+        //Initializing an AlertDialog and building a dialog that allows the user to write a category name
         val dialog = AlertDialog.Builder(this)
             .setTitle("New category")
             .setMessage("Enter the name of the category you want to create")
             .setView(inputEditTextField)
             .setPositiveButton("Create") { _, _ ->
-                if (inputEditTextField.text.toString() == "") {
-                    Toast.makeText(this, "The category wasnt created as the name was empty", Toast.LENGTH_LONG).show()
+                //Checking if the input is empty and handling it accordingly
+                if (userInput == "") {
+                    Toast.makeText(this, "The category wasn't created. Reason: Name was empty", Toast.LENGTH_LONG).show()
+                    return@setPositiveButton
+                }
+                //Checking if there exists a category with the same name as the user input
+                if (checkIfCategoryExists(categoriesArrayList, userInput)) {
+                    Log.e("OH NO","IT CONTAINS")
+                    Toast.makeText(this, "The category wasn't created. Reason: There is already exists a category with that name", Toast.LENGTH_LONG).show()
                     return@setPositiveButton
                 }
                 //Getting the categories array listed from the userCollective cache data
@@ -127,54 +121,83 @@ class AddTaskActivity : AppCompatActivity() {
                 this.onStop()
                 //Storing the categoriesArrayList to the DB
                 Database().updateValueInDB("collective",collectiveID.toString(),"categories",categoriesArrayList,null)
-
-
-
-
             }
             .setNegativeButton("Cancel", null)
             .create()
         dialog.show()
 
-
-
     }
 
+    /**
+     * A function that checks if there is a category existing with the same name. The check is case insensitive
+     * @param list the arraylist which holds all the categories to check in
+     * @param nameToCheck the name of the category we want to check for
+     */
+    private fun checkIfCategoryExists(list: ArrayList<String>, nameToCheck : String) : Boolean{
+        //Iterating through the array list
+        for (i in list) {
+            //Checking if the value in the list is the same as the value we are checking for. Case insensitive
+            if (i.lowercase() == nameToCheck.lowercase() ) {
+                return true
+            }
+        }
+        return false
+    }
 
+    /**
+     * A function that creates the task by adding all the given task information by the user into a map
+     * and adding that map to the task arraylist and updating the database accordingly
+     */
     private fun createTask() {
-        val userID = intent.getStringExtra("uid")
+        //Checking if the given username is empty and handling it accordingly
         if (taskName.text.toString() == "") {
             Toast.makeText(this, "Please write a task name in order to create the task", Toast.LENGTH_LONG).show()
-        }
-        //Retrieving the task array stored in the collective DB cache
-        var tasksArray : ArrayList<MutableMap<String,String>>
-        if (userCollectiveData[0]?.data?.get("tasks") == null) {
-            tasksArray = ArrayList<MutableMap<String,String>>()
-            Log.d("AddTaskActivity()","No tasks in the collective")
-
-        }
-        else {
-            tasksArray = userCollectiveData[0]?.data?.get("tasks") as ArrayList<MutableMap<String, String>>
-        }
-        //Creating a map for tasks
-        val task = mutableMapOf<String,String>()
-        //Adding the task information into the map
-
-        task["name"] = taskName.text.toString()
-        task["description"] = taskDescription.text.toString()
-        task["dueDate"] = taskDueDate.text.toString()
-
-        //Adding the task to the task array
-        tasksArray.add(task)
-        //Creating a hashmap with the field tasks and the value is the tasksArray
-        val addDataToDB = hashMapOf(
-            "tasks" to tasksArray
-        )
-        if (userID == null) {
-            Log.d("Create task: Error", "the userID is null")
             return
         }
+        //Initializing a mutable map used to store the members assigned or non assigned members
+        val assignedMembers : MutableMap<String, Boolean> = mutableMapOf()
+
+        //Creating a variable of ArrayList<MutableMap<String,Any>>
+        var tasksArray : ArrayList<MutableMap<String,Any>>
+
+        //Checking if the collectiveData cache doesn't have tasks data
+        if (userCollectiveData[0]?.data?.get("tasks") == null) {
+            //Initializing the tasksArray variable with an empty arraylist
+            tasksArray = ArrayList<MutableMap<String,Any>>()
+            Log.d("AddTaskActivity()","No tasks in the collective")
+        }
+        else {
+            //Initializing the tasksArray variable with the task data from the collectiveData cache
+            tasksArray = userCollectiveData[0]?.data?.get("tasks") as ArrayList<MutableMap<String, Any>>
+        }
+
+        //Initializing a mutable map for the task information
+        val taskInformation = mutableMapOf<String,Any>()
+
+        //Iterating through the assignCollectiveMembersListView and checking which members got assigned
+        for (i in 0 until assignCollectiveMembersListView.size) {
+            //Initializing the name of the member retrieved from the assignCollectiveMembersListView
+            val memberName :String = assignCollectiveMembersListView.getItemAtPosition(i).toString()
+            //Initializing the boolean value of the member retrieved from the assignCollectiveMembersListView
+            val memberIsAssigned : Boolean = assignCollectiveMembersListView.isItemChecked(i)
+            //Adding the member to a map with the value true or false depending on if the user got clicked on in the list
+            assignedMembers[memberName] = memberIsAssigned
+
+        }
+        //Adding all the task information to the task map
+        taskInformation["name"] = taskName.text.toString()
+        taskInformation["description"] = taskDescription.text.toString()
+        taskInformation["dueDate"] = taskDueDate.text.toString()
+        taskInformation["assigned"] = assignedMembers
+        taskInformation["category"] = taskCategories.selectedItem.toString()
+
+
+        //Adding the task information to the task array which holds all the collective's tasks
+        tasksArray.add(taskInformation)
+
+        //Initializing a variable with the collectiveID found in the userData
         val collectiveID = userData[0]?.data?.get("collectiveID").toString()
+
         //Adding the new task array to the DB
         Database().updateValueInDB("collective",collectiveID,"tasks",tasksArray,null)
 
