f4e7e14ae7dca29586b074ce00ff7d91eae3a609
Vithujan Paskaralingam <vithu1021@hotmail.com>
2022-11-16 23:57:35 +0100

    Created a utilities file and created a function to build an alert dialog

---
+++ app/src/main/java/com/example/collectivecleaningorganizer/ui/utilities/utilities.kt
@@ -0,0 +1,22 @@
+package com.example.collectivecleaningorganizer.ui.utilities
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.View
+
+class utilities {
+    /**
+     * A function that builds an alert dialog
+     * @param context is the context we want to show the dialog
+     * @param title is the title of the alert dialog
+     * @param message is the message which is shown in the alert dialog
+     * @param view is the layout widgets we want to add to the dialog if there any
+     */
+    fun alertDialogBuilder(context : Context, title: String?, message: String?, view: View?): AlertDialog.Builder {
+
+        return AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setView(view)
+    }
+}
