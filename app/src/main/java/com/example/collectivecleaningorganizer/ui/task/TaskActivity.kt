26ce85b284330576c7caef4bd6e9114a45056192
Lars Blütecher Holter <larsbho@stud.ntnu.no>
2022-11-20 00:25:36 +0100

    Assigned now fixed how looks in TaskActivity

--- app/src/main/java/com/example/collectivecleaningorganizer/ui/task/TaskActivity.kt
+++ app/src/main/java/com/example/collectivecleaningorganizer/ui/task/TaskActivity.kt
@@ -9,6 +9,7 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.collectivecleaningorganizer.R
+import kotlinx.android.synthetic.main.activity_edit_task.*
 import kotlinx.android.synthetic.main.activity_view_task.*
 import kotlinx.android.synthetic.main.activity_view_task.assignCollectiveMembersListView
 import kotlinx.android.synthetic.main.activity_view_task.back_btn
@@ -36,14 +37,19 @@ class TaskActivity : AppCompatActivity() {
         taskName.text           = intent.getStringExtra("name").toString()
         taskDueDate.text        = intent.getStringExtra("dueDate").toString()
         taskDescription.text    = intent.getStringExtra("description").toString()
+        val assigned : ArrayList<String>   = intent.getStringArrayListExtra("assigned") as ArrayList<String>
 
         val listOfAssigned = mutableListOf<String>()
         listOfAssigned.add(intent.getStringExtra("assigned").toString())
 
-        val membersAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,listOfAssigned)
+        val membersAdapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,assigned)
         //Assigning the adapter to the ListView with the id "assignCollectiveMembersListView"
         assignCollectiveMembersListView.adapter = membersAdapter
 
+        for (i : Int in 0 until assigned.size) {
+            assignCollectiveMembersListView.setItemChecked(i,true)
+        }
+        assignCollectiveMembersListView.isEnabled = false
 
 
         val categories = mutableListOf<String>()
@@ -54,8 +60,8 @@ class TaskActivity : AppCompatActivity() {
 
 
         editTask_btn.setOnClickListener{
-            Log.e("NAME:", taskName.text.toString())
-            editTaskPage(taskName.text.toString(),taskDueDate.text.toString(), taskDescription.text.toString(), "task[].toString()", "task[].toString()", userID)
+            Log.e("NAME:", assigned.toString())
+            editTaskPage(taskName.text.toString(),taskDueDate.text.toString(), taskDescription.text.toString(), intent.getStringExtra("category").toString(), assigned, userID)
         }
 
 
@@ -68,7 +74,7 @@ class TaskActivity : AppCompatActivity() {
     }
 
 
-    private fun editTaskPage(name : String, dueDate : String, description : String, category : String, assigned : String, userID: String) {
+    private fun editTaskPage(name : String, dueDate : String, description : String, category : String, assigned : ArrayList<String>, userID: String) {
         val newIntent = Intent(this, EditTaskActivity::class.java)
 
         newIntent.putExtra("uid",userID)
@@ -77,6 +83,8 @@ class TaskActivity : AppCompatActivity() {
         newIntent.putExtra("description", description)
         newIntent.putExtra("assigned", assigned)
         newIntent.putExtra("category", category)
+        newIntent.putExtra("index", intent.getIntExtra("index",0))
+
 
         startActivity(newIntent)
     }
